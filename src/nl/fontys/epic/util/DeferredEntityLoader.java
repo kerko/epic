cc36a4d87d5197566534d1a35e1e3833ceaea5de
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2013-12-03 14:20:00 +0100

    Added DeferredEntityStorage

--- src/nl/fontys/epic/util/DeferredEntityLoader.java
+++ src/nl/fontys/epic/util/DeferredEntityLoader.java
@@ -19,119 +19,45 @@
  * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
+
 package nl.fontys.epic.util;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
+import nl.fontys.epic.TextAdventure;
 
 /**
- * Loads entities into a target later on
+ * Loaders deferred data into the entities
  *
  * @author Miguel Gonzalez <miguel-gonzalez@gmx.de>
  * @since 1.0
  * @version 1.0
  */
 public class DeferredEntityLoader {
-
-    public static enum EntityType {
-
-        CREATURE,
-        ITEM
+    
+    public void load(TextAdventure adventure) throws LoadingException {
+        
+        ResourceManager resourceManager = SharedResourceManager.getInstance(adventure.getName());
+        DeferredEntityStorage storage =   DeferredEntityStorage.getInstance();
+        
+        // Fill up all rooms
+        //for (Room room : adventure.getRooms()) {
+            
+       // }
     }
     
-    public static class DeferredData {
-             
-        public String id;        
-        public EntityType type;
-        public Position pos;
-        
-        public DeferredData(String id, EntityType type, int x, int y) {
-            this.id = id;
-            this.type = type;
-            this.pos = new Position(x, y);
+    public static class LoadingException extends Exception {
+
+        public LoadingException(String message) {
+            super(message);
         }
 
-        @Override
-        public int hashCode() {
-            int hash = 7;
-            hash = 37 * hash + Objects.hashCode(this.id);
-            hash = 37 * hash + Objects.hashCode(this.type);
-            hash = 37 * hash + Objects.hashCode(this.pos);
-            return hash;
+        public LoadingException(String message, Throwable cause) {
+            super(message, cause);
         }
 
-        @Override
-        public boolean equals(Object obj) {
-            if (obj == null) {
-                return false;
-            }
-            if (getClass() != obj.getClass()) {
-                return false;
-            }
-            final DeferredData other = (DeferredData) obj;
-            if (!Objects.equals(this.id, other.id)) {
-                return false;
-            }
-            if (this.type != other.type) {
-                return false;
-            }
-            if (!Objects.equals(this.pos, other.pos)) {
-                return false;
-            }
-            return true;
+        public LoadingException(Throwable cause) {
+            super(cause);
         }
         
         
-    }
-
-    private static final DeferredEntityLoader instance;
-    
-    private final Map<String, List<DeferredData> > data;
-
-    static {
-        instance = new DeferredEntityLoader();
-    }
-
-    private DeferredEntityLoader() {
-        data = new HashMap< >();
-    }
-
-    public static DeferredEntityLoader getInstance() {
-        return instance;
-    }
-
-    public void add(String parent, String id, EntityType type, int x, int y) {
-        DeferredData element = new DeferredData(id, type, x, y);        
-        List<DeferredData> dataList = data.get(parent);
-        
-        if (dataList == null) {
-            dataList = new ArrayList< >();
-            data.put(parent, dataList);
-        }        
-        
-        if (!dataList.contains(element)) {
-            dataList.add(element);
-        }
-    }
-
-    public void add(String parent, String id, EntityType type) {
-        add(parent, id, type, -1, -1);
-    }
-    
-    public DeferredData fetch(String parent) {
-        List<DeferredData> list = data.get(parent);        
-        DeferredData element = list.remove(0);
-        if (list.isEmpty()) {
-            data.remove(parent);
-        }
-        
-        return element;
-    }
-    
-    public boolean hasNext(String parent) {
-        return data.get(parent) != null;
     }
 }
