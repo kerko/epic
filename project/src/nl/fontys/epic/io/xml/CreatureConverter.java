1377f29919885feb73d176f1ab464bef9e7f1629
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2014-01-07 12:41:03 +0100

    Added javadoc

---
+++ project/src/nl/fontys/epic/io/xml/CreatureConverter.java
@@ -0,0 +1,49 @@
+/* The MIT License (MIT)
+ * 
+ * Copyright (c) 2013 Jan Kerkenhoff, Miguel Gonzalez
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package nl.fontys.epic.io.xml;
+
+import nl.fontys.epic.core.Creature;
+import nl.fontys.epic.io.ContentConverter;
+import nl.fontys.epic.io.ConvertException;
+import org.w3c.dom.Node;
+
+/**
+ * Converts objects of {@see Creature} to XML nodes and vise versa
+ * 
+ * @author Miguel Gonzalez <miguel-gonzalez@gmx.de>
+ * @since 1.0
+ * @version 1.0
+ */
+public class CreatureConverter implements ContentConverter<Node, Creature> {
+
+    @Override
+    public Node toOutput(Creature source) throws ConvertException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Creature toInput(Node source) throws ConvertException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
